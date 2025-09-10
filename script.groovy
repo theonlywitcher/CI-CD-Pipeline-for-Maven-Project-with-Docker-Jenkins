acfec33029c76717aef094f76f81b7a1a28aa6b5
AhmedReda <ahmedreda4014@gmail.com>
2025-09-10 22:39:38 +0300

    fixed jenkins

--- script.groovy
+++ script.groovy
@@ -11,10 +11,10 @@ def testapp() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-accout', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t ahmedredadev/jave-maven-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push ahmedredadev/jave-maven-app:jma-2.0'
     }
 } 
 
