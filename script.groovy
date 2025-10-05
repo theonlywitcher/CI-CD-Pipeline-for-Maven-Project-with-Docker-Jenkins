c8562fcc6d1c406e468c039d969324f1163ad2dc
AhmedReda <ahmedreda4014@gmail.com>
2025-10-05 20:08:56 +0300

    edit

--- script.groovy
+++ script.groovy
@@ -29,7 +29,7 @@ def buildImage() {
 def deployApp() {
     dockerCmd = "docker run -d -p 8080:8080 --name jave-maven-app-${IMAGE_NAME} ahmedredadev/jave-maven-app:${IMAGE_NAME}"
     sshagent(['Ec2-Server']) {
-    sh 'ssh -o StrictHostKeyChecking=no ec2-user@98.81.215.132'
+    sh 'ssh -o StrictHostKeyChecking=no ec2user@98.81.215.132'
     }
 } 
 
